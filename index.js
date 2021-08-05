96681ec0620f61a87ced31a87a321b91cb8473b5
Abel Perez <abel.perez1342@gmail.com>
2021-08-05 11:56:36 -0400

    first commit

--- index.js
+++ index.js
@@ -1,41 +1,31 @@
-const express = require("express");
-const faker = require("faker");
-const _ = require("lodash");
-
+const express = require('express');
 const app = express();
-const port = 8000;
-
-const fakeReminders = faker.git.commitMessage();
-
-app.get("/reminder", (req, res) => {
+const faker = require('faker');
+const _ = require('lodash');
+const port = 8001;
+const fakerReminders = faker.lorem.text();
+app.get('/',(req, res) => {
   const count = 1;
-  if (!count) {
-    console.log("in 400 error");
-    return res
-      .status(400)
-      .send({ errorMsg: "count query parameter is missing" });
-  } else if (fakeReminders) {
-    console.log("in 200 status");
+  if(!count) {
+    console.log('in 400 error');
+    return res.status(400).send({errorMsg:'count query parameter is missing'});
+  }else if(fakerReminders) {
+    console.log('in 200 status');
     const uuid = faker.datatype.uuid();
     const reminderObject = {
       id: uuid,
-      reminder: fakeReminders,
-    };
+      reminder: fakerReminders,
+    }
     console.log(reminderObject);
     res.status(200).json(reminderObject);
-  } else {
-    console.log("in 500 error.");
-    (err) => {
-      console.log("error", err);
-      res
-        .status(500)
-        .json({
-          message: "error, server has crashed running getting all reminders",
-        });
-    };
+  }else {
+    console.log('in 500 error');
+    err => {
+      console.log('error', err);
+      res.status(500).json({message: "error, server has crashed running get all reminders"});
+    }
   }
-});
-
+})
 app.listen(port, () => {
-  console.log(`Reminder app is listening on http://localhost:${port}`);
+  console.log(`====Example app listening at http://localhost:${port}=====`);
 });
